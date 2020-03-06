0b4814ac0cb603afde346c2df3c7a7da9d546841
Quentin Schifferle <q.schifferle@coprotec.net>
2020-03-06 16:52:46 +0100

    Modification des tables projets et utilisateurs

--- utilisateurs.sql
+++ utilisateurs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 03 mars 2020 à 21:28
--- Version du serveur :  5.7.26
--- Version de PHP :  7.2.18
+-- Hôte : 127.0.0.1:3307
+-- Généré le :  ven. 06 mars 2020 à 15:52
+-- Version du serveur :  10.2.14-MariaDB
+-- Version de PHP :  7.1.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `polyphonia`
+-- Base de données :  `test`
 --
 
 -- --------------------------------------------------------
@@ -32,7 +32,8 @@ DROP TABLE IF EXISTS `utilisateurs`;
 CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom_utilisateur` varchar(255) NOT NULL,
-  `date_inscription` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `pass` text NOT NULL,
+  `date_inscription` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
@@ -40,8 +41,8 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
 -- Déchargement des données de la table `utilisateurs`
 --
 
-INSERT INTO `utilisateurs` (`id`, `nom_utilisateur`, `date_inscription`) VALUES
-(1, 'syngates68', '2020-03-03 21:03:34');
+INSERT INTO `utilisateurs` (`id`, `nom_utilisateur`, `pass`, `date_inscription`) VALUES
+(1, 'syngates68', '$2y$10$EWs0.V.uJ2ajUorWRECawe5oqb0GY9kTvT/e1si0stbWONSSMpU1y', '2020-03-03 21:03:34');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
