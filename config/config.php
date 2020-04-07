7774fec52acf2d4d5caf1cc4663373c320700598
syngates68 <quentin.schifferle@gmail.com>
2020-04-07 22:20:59 +0200

    Auto stash before merge of "master" and "origin/master"

--- config/config.php
+++ config/config.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< Updated upstream
 define("BASEURL", "http://localhost/polyphonia/");
 $var_page = (isset($_GET['page'])) ? $_GET['page'] : 'accueil';
 
@@ -10,3 +11,7 @@ $pages_admin = [
     "nouveau_projet",
     "liste_utilisateurs"
 ];
+=======
+define("BASEURL", "http://192.168.1.41/polyphonia_new/");
+$var_page = (isset($_GET['page'])) ? $_GET['page'] : 'accueil';
+>>>>>>> Stashed changes
