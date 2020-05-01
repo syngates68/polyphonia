8c30b8ec9328049e90c2eb9be7f5881ec3d92a3c
Quentin <quentin.schifferle@gmail.com>
2020-05-01 23:53:37 +0200

    Ajout/Modification tables

--- fichiers_projet.sql
+++ fichiers_projet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 23 avr. 2020 à 19:38
+-- Généré le :  ven. 01 mai 2020 à 23:53
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -25,28 +25,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `messagerie`
+-- Structure de la table `fichiers_projet`
 --
 
-DROP TABLE IF EXISTS `messagerie`;
-CREATE TABLE IF NOT EXISTS `messagerie` (
+DROP TABLE IF EXISTS `fichiers_projet`;
+CREATE TABLE IF NOT EXISTS `fichiers_projet` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_utilisateur_1` int(11) NOT NULL,
-  `id_utilisateur_2` int(11) NOT NULL,
-  `date_ouverture` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_projet` int(11) NOT NULL,
+  `chemin_fichier` varchar(255) NOT NULL,
+  `nom_fichier` varchar(255) NOT NULL,
+  `type_fichier` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `messagerie`
+-- Déchargement des données de la table `fichiers_projet`
 --
 
-INSERT INTO `messagerie` (`id`, `id_utilisateur_1`, `id_utilisateur_2`, `date_ouverture`) VALUES
-(2, 1, 2, '2020-04-20 17:21:42'),
-(4, 6, 1, '2020-04-20 21:08:37'),
-(5, 3, 1, '2020-04-20 21:45:16'),
-(6, 1, 7, '2020-04-20 23:36:25'),
-(7, 1, 8, '2020-04-20 23:42:02');
+INSERT INTO `fichiers_projet` (`id`, `id_projet`, `chemin_fichier`, `nom_fichier`, `type_fichier`) VALUES
+(1, 25, 'assets/projets/fichiers/2048.html', 'Template de jeu', 'text/html');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
